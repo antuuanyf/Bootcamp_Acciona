bb377a6b52478e23ea20a2c5ffa665b852793b25
Antonio Monreal Diaz <59146049+antuuanyf@users.noreply.github.com>
2023-07-03 15:53:33 +0200

    Add files via upload

---
+++ Day_1/1. ThirdAngleOfATriangle.js
@@ -0,0 +1,14 @@
+//You are given two interior angles (in degrees) of a triangle.
+
+//Write a function to return the 3rd.
+
+//Note: only positive integers will be tested.
+
+//https://en.wikipedia.org/wiki/Triangle
+
+function otherAngle(a, b) {
+    let fResult = null
+    let r1 = a + b
+    fResult = 180 - r1
+    return fResult;
+  }
